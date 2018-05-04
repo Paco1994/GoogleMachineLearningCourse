29b8c6e5ab9377ec474223fcf0a1aa6758efa338
Paco1994 <i32rodrf@uco.es>
2018-05-04 16:23:33 +0200

    Broadcasting example

---
+++ tensorflow/3_creating_and_manipulating_tensors/3_broadcasting.py
@@ -0,0 +1,17 @@
+# The result is the same as in 1_vector_addition.py
+# This is broadcasting and the other example is with a simple sum
+
+import tensorflow as tf
+
+with tf.Graph().as_default():
+  # Create a six-element vector (1-D tensor).
+  primes = tf.constant([2, 3, 5, 7, 11, 13], dtype=tf.int32)
+
+  # Create a constant scalar with value 1.
+  ones = tf.constant(1, dtype=tf.int32)
+
+  # Add the two tensors. The resulting tensor is a six-element vector.
+  just_beyond_primes = tf.add(primes, ones)
+
+  with tf.Session() as sess:
+    print (just_beyond_primes.eval())
